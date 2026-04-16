521092f849cebd950cc5d5b24b5d537848dc3b02
danilui28 <david82.macias@gmail.com>
2026-04-16 13:11:59 -0500

    creacion personaje

--- limones.js
+++ limones.js
@@ -1,2 +1,21 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const alturaSuelo = 40;
+const alturaPersonaje = 60;
+const anchoPersonaje = 40;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo(){
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,canvas.height-40,canvas.width,40)
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(alturaSuelo+alturaPersonaje),anchoPersonaje,alturaPersonaje)
+}
